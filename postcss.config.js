ef2e525070bb10171e7344b40f1b19f65e1fca4e
Angela Langat <angela.langat@yahoo.com>
2025-01-01 23:25:09 +0300

    Add npm and postcss configurations

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-  plugins: {
-    'postcss-import': {},
-    'tailwindcss/nesting': {},
-    tailwindcss: {},
-    autoprefixer: {
-      flexbox: 'no-2009'
-    },
-    ...(process.env.HUGO_ENVIRONMENT === 'production' ? {
-      '@fullhuman/postcss-purgecss': {
-        content: ['./hugo_stats.json'],
-        defaultExtractor: (content) => {
-          let els = JSON.parse(content).htmlElements;
-          return els.tags.concat(els.classes, els.ids);
+    plugins: {
+      'postcss-import': {},
+      'tailwindcss/nesting': {},
+      tailwindcss: {},
+      autoprefixer: {
+        flexbox: 'no-2009'
+      },
+      ...(process.env.HUGO_ENVIRONMENT === 'production' ? {
+        '@fullhuman/postcss-purgecss': {
+          content: ['./hugo_stats.json'],
+          defaultExtractor: (content) => {
+            let els = JSON.parse(content).htmlElements;
+            return els.tags.concat(els.classes, els.ids);
+          }
         }
-      }
-    } : {})
+      } : {})
+    }
   }
-}
